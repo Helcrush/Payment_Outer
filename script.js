d5e04715a37d72d36fa752fd43b31d6e8dd6688e
Helcrush <helcrush001@gmail.com>
2025-07-05 04:02:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 function startQRISPayment() {
+
     // Simulate payment logic for QRIS
-    alert("Apakah anda yakin ingin beralih ke qris?");
+
+    alert("𝘼𝙣𝙙𝙖 𝙔𝙖𝙠𝙞𝙣 𝙄𝙣𝙜𝙞𝙣 𝘽𝙚𝙧𝙥𝙞𝙣𝙙𝙖𝙝?");
+
     // You can replace this with actual payment logic
+
     window.location.href = "payqris.html"; // Change to actual QRIS payment URL
+
 }
+
+
 
 function startDanaPayment() {
+
     // Simulate payment logic for DANA
-    alert("Apakah anda yakin ingin beralih ke dana?");
+
+    alert("𝘼𝙣𝙙𝙖 𝙔𝙖𝙠𝙞𝙣 𝙄𝙣𝙜𝙞𝙣 𝘽𝙚𝙧𝙥𝙞𝙣𝙙𝙖𝙝?");
+
     // You can replace this with actual payment logic
+
     window.location.href = "paydana.html"; // Change to actual DANA payment URL
+
 }
+
+
 
 function startGopayPayment() {
+
     // Simulate payment logic for GOPAY
-    alert("Apakah anda yakin ingin beralih ke gopay?");
+
+    alert("𝘼𝙣𝙙𝙖 𝙔𝙖𝙠𝙞𝙣 𝙄𝙣𝙜𝙞𝙣 𝘽𝙚𝙧𝙥𝙞𝙣𝙙𝙖𝙝?");
+
     // You can replace this with actual payment logic
+
     window.location.href = "paygopay.html"; // Change to actual GOPAY payment URL
+
 }
 
+
+
 function startOvoPayment() {
+
     // Simulate payment logic for OVO
-    alert("Redirecting to GOPAY payment...");
+
+    alert("𝘼𝙣𝙙𝙖 𝙔𝙖𝙠𝙞𝙣 𝙄𝙣𝙜𝙞𝙣 𝘽𝙚𝙧𝙥𝙞𝙣𝙙𝙖𝙝?");
+
     // You can replace this with actual payment logic
+
     window.location.href = "payovo.html"; // Change to actual OVO payment URL
+
 }
+
